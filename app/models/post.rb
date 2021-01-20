37c301d40ea6bccfa4ca90400709f66a4575a651
tavo <gasb_150@hotmail.com>
2021-01-20 17:15:20 -0500

    add post and user validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :title, presence: true
+  validates :title, length: { maximun: 50 }
+  validates :description, presence: true
 end
