3808683e89cd283584720bc1a672190bf7b59815
milypm <mp_morones@hotmail.com>
2021-01-20 11:12:28 -0600

    Create Comment model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :posts
-    validates :username, length: { minimum: 4}
+    has_many :comments
+    validates :username, length: { minimum: 4 }
     validates :username, presence: true
     validates :username, uniqueness: true
 end
