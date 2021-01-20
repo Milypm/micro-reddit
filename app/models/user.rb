878aba6d3df2432ce64e6c64220d8f040224558c
tavo <gasb_150@hotmail.com>
2021-01-20 11:43:32 -0500

    add post model with relation bewtwen user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
     validates :username, length: { minimum: 4}
     validates :username, presence: true
     validates :username, uniqueness: true
